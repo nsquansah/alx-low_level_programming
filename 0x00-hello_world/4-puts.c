9cafa452bec5a81c5f843bdd10b8f35d88a70bdf
nsquansah <ninaspioquasah@gmail.com>
2022-09-08 16:47:39 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
+/**
+ * main - main block
+ * Return: 0
+ */
 
-/* function returning an int */
-int main (void)
-{
-	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
-
+int main(void)
+{	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
