266a64e1af63bac009666abd6b9dbeaee0c3082c
nsquansah <ninaspioquasah@gmail.com>
2022-11-01 13:49:46 -0700

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,43 +1,31 @@
-#include <stdio.h>
-#include "holberton.h"
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <unistd.h>
-#include <stdlib.h>
+#include "main.h"
 /**
- * read_textfile - Reads a text file and prints it to the POSIX
- * standard output.
- * @filename: file.
- * @letters: Number of letters it should read and print.
- * Return: Actual number of letters it could read and print.
+ * read_textfile - reads a textfile
+ * @filename: file to read
+ * @letters: amount of bytes to read
+ * Return: printed letters count
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd, res_read, res_write;
-	char *buf;
+	int fd, printed, wrote;
+	char *buff;
 
+	buff = malloc(letters);
+	if (buff == NULL)
+		return (0);
 	if (filename == NULL)
 		return (0);
-	fd = open(filename, O_RDONLY);
+	fd = open(filename, O_RDWR);
 	if (fd == -1)
 		return (0);
-	buf = malloc(sizeof(char) * letters);
-	if (buf == NULL)
+	printed = read(fd, buff, letters);
+	if (printed == -1)
 		return (0);
-	res_read = read(fd, buf, letters);
-	if (res_read == -1)
-	{
-		free(buf);
+	wrote = write(STDOUT_FILENO, buff, printed);
+	if (wrote == -1)
 		return (0);
-	}
-	res_write = write(STDOUT_FILENO, buf, res_read);
-	if (res_write == -1 || res_read != res_write)
-	{
-		free(buf);
+	if (close(fd) == -1)
 		return (0);
-	}
-	free(buf);
-	close(fd);
-	return (res_write);
+	free(buff);
+	return (printed);
 }
